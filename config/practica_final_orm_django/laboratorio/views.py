9c854b14f53c067fde66d5c8190ab11f45ebeb6d
kenkairon <ingeinfo2006@gmail.com>
2024-12-29 01:56:18 -0300

    actualizar el views listar laboratorio para enumerar y agregar paginación

--- config/practica_final_orm_django/laboratorio/views.py
+++ config/practica_final_orm_django/laboratorio/views.py
@@ -1,25 +1,35 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Laboratorio
 from .forms import LaboratorioForm
+from django.core.paginator import Paginator
+from django.db.models import IntegerField
+from django.db.models.functions import Cast, Substr
 
-# 1. Listar laboratorios
-from django.shortcuts import render
-
+# 2. Listar laboratorio
 def laboratorio_list(request):
     # Obtener el contador de visitas de las cookies del usuario
     visitas = int(request.COOKIES.get('visitas', 0)) + 1
 
-    # Obtener la lista de laboratorios desde el modelo y ordenarlos por el campo deseado   
-    laboratorios = Laboratorio.objects.all().order_by('nombre')  # Cambia 'nombre' por el campo deseado
+    # Anotar el número extraído y ordenar por este valor
+    laboratorios = Laboratorio.objects.annotate(
+        numero=Cast(Substr('nombre', 12), IntegerField())
+    ).order_by('numero')
 
-    # Renderizar la página y pasar el contador como contexto
-    response = render(request, 'laboratorios/laboratorio_list.html', {'laboratorios': laboratorios, 'visitas': visitas})
-    
+    # Implementar paginación (10 elementos por página)
+    paginator = Paginator(laboratorios, 5)
+    page_number = request.GET.get('page')  # Obtener el número de página desde la URL
+    page_obj = paginator.get_page(page_number)
+
+    # Renderizar la página
+    response = render(request, 'laboratorios/laboratorio_list.html', {
+        'page_obj': page_obj,
+        'visitas': visitas,
+    })
+
     # Configurar la cookie de visitas
     response.set_cookie('visitas', visitas, max_age=60*60*24*30)  # 30 días de duración
-    
-    return response
 
+    return response
 
 # 2. Crear un nuevo laboratorio
 def laboratorio_create(request):
